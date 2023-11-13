532c9fef00e70aaee7ec6024bbf21256c49ce423
Gusutoaie Ovidiu Marius <ovidiu.gusutoaie95@e-uvt.ro>
2023-11-13 23:14:16 +0200

    Added security + controllers

--- SportsSync_BE/src/main/java/com/example/sportssync_be/entities/User.java
+++ SportsSync_BE/src/main/java/com/example/sportssync_be/entities/User.java
@@ -1,11 +1,19 @@
 package com.example.sportssync_be.entities;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.Collection;
 import java.util.List;
 
 @Entity
-public class User {
+@Data
+public class User implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -32,10 +40,49 @@ public class User {
     @Column
     private String gender;
 
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
     @OneToMany(mappedBy = "organizer")
     private List<Event> eventList;
 
     @OneToMany(mappedBy = "user")
     private List<Entry> entryList;
 
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+
+    }
+
+    @Override
+    public String getPassword() {
+        return this.password;
+
+    }
+
+    @Override
+    public String getUsername() {
+        return this.username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
 }
---
+++ SportsSync_BE/src/main/java/com/example/sportssync_be/service/UserService.java
@@ -0,0 +1,24 @@
+package com.example.sportssync_be.service;
+
+import com.example.sportssync_be.entities.User;
+import com.example.sportssync_be.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+@Service
+public class UserService  {
+
+  private final UserRepository userRepository;
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public boolean isValidUser(String username, String password) {
+        Optional<User> userOptional = userRepository.findByUsernameAndPassword(username, password);
+        return userOptional.isPresent();
+    }
+}
