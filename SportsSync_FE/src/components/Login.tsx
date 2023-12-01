bd316f30bb05b6b22f1b7123f5553683cfe5c4db
Smarandache Grigorie-Theodor <grigorie.smarandache02@e-uvt.ro>
2023-12-01 16:24:47 +0200

    Login complete

--- SportsSync_FE/src/components/Login.tsx
+++ SportsSync_FE/src/components/Login.tsx
@@ -9,6 +9,10 @@ import Grid from "@mui/material/Grid";
 // CSS
 import LoginCSS from "./style/Login.module.css";
 
+
+// Images
+import Logo from "./images/logo.png";
+import Footballer from "./images/footballer.png";
 function Login() {
     const navigate = useNavigate();
 
@@ -38,60 +42,72 @@ function Login() {
         <Box className={LoginCSS.background}>
             <Grid container xs={10} md={8} className={LoginCSS.container}>
                 <Grid container xs={12} md={6} className={LoginCSS.left_column}>
-                    <img
-                        src={require("./images/fotbal.png")}
-                        alt="fotbal"
-                        className={LoginCSS.image}
-                    />
-                </Grid>
-
-                <Grid container xs={12} md={6} className={LoginCSS.right_column}>
-                    <Grid item xs={12} md={12} className={LoginCSS.item}>
-                        <h1 className={LoginCSS.title}> Login</h1>
-                    </Grid>
 
                     <Grid item xs={12} md={12} className={LoginCSS.item}>
-                        <input
-                            type="text"
-                            placeholder="Email"
-                            className={LoginCSS.input}
-                            value={emailAddress}
-                            onChange={(event) =>
-                                setEmailAddress(event.target.value)
-                            }
-                        ></input>
-                    </Grid>
-
-                    <Grid item xs={12} md={12} className={LoginCSS.item}>
-                        <input
-                            type="password"
-                            placeholder='Password'
-                            className={LoginCSS.input}
-                            value={password}
-                            onChange={(event) =>
-                                setPassword(event.target.value)
-                            }
-                        ></input>
-                    </Grid>
-                    <Grid item xs={12} md={12} className={LoginCSS.item}>
-                        <p className={LoginCSS.text_error}> {errorMessage} </p>
-                    </Grid>
-                    <Grid item xs={12} md={12} className={LoginCSS.item}>
-                        <button className={LoginCSS.button} onClick={handleLogin}>
-                            Login
+                        <button className={LoginCSS.home_button} onClick={() => navigate("/home")}>
+                            <img
+                                src={Logo}
+                                alt="Logo"
+                                className={LoginCSS.logo}
+                            ></img>
+                            <p className={LoginCSS.app_title}>SportSync</p>
                         </button>
                     </Grid>
+
                     <Grid item xs={12} md={12} className={LoginCSS.item}>
-                        <p className={LoginCSS.text}>
-                            You don't have an account?{" "}
-                            <Link to="/Register" className={LoginCSS.link}>
-                                Register here
-                            </Link>
-                        </p>
+                        <img src={Footballer}
+                            alt="footballer"
+                            className={LoginCSS.image}
+                        ></img>
                     </Grid>
                 </Grid>
+            <Grid container xs={12} md={6} className={LoginCSS.right_column}>
+                <Grid item xs={12} md={12} className={LoginCSS.item}>
+                    <h1 className={LoginCSS.title}> Login</h1>
+                </Grid>
+
+                <Grid item xs={12} md={12} className={LoginCSS.item}>
+                    <input
+                        type="text"
+                        placeholder="Email"
+                        className={LoginCSS.input}
+                        value={emailAddress}
+                        onChange={(event) =>
+                            setEmailAddress(event.target.value)
+                        }
+                    ></input>
+                </Grid>
+
+                <Grid item xs={12} md={12} className={LoginCSS.item}>
+                    <input
+                        type="password"
+                        placeholder='Password'
+                        className={LoginCSS.input}
+                        value={password}
+                        onChange={(event) =>
+                            setPassword(event.target.value)
+                        }
+                    ></input>
+                </Grid>
+                <Grid item xs={12} md={12} className={LoginCSS.item}>
+                    <p className={LoginCSS.text_error}> {errorMessage} </p>
+                </Grid>
+                <Grid item xs={12} md={12} className={LoginCSS.item}>
+                    <button className={LoginCSS.button} onClick={handleLogin}>
+                        Login
+                    </button>
+                </Grid>
+                <Grid item xs={12} md={12} className={LoginCSS.item}>
+                    <p className={LoginCSS.text}>
+                        You don't have an account?{" "}
+                        <Link to="/Register" className={LoginCSS.link}>
+                            Register here
+                        </Link>
+                    </p>
+                </Grid>
             </Grid>
-        </Box>
+        </Grid>
+        </Box >
     );
 };
 
